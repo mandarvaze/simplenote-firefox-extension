b9962d5f83646494f62d05ad4bb3a172a841bc36
Mandar Vaze <mandarvaze@gmail.com>
2011-08-22 23:39:51 +0530

    Login to simplenote and creating new notes works.

--- chrome/content/simplenote.js
+++ chrome/content/simplenote.js
@@ -1,14 +1,97 @@
-function onLoad(){
-//    alert("Load");
-}
+var simplenotePrefs = {
+   PREFERENCES_ROOT     : "extensions.simplenote.",
+
+   init : function() {
+      // connect to the preferences system
+      this.root = Components.classes["@mozilla.org/preferences-service;1"].
+                     getService(Components.interfaces.nsIPrefService).
+                     getBranch(this.PREFERENCES_ROOT);
+      //this.root.setString("authtok", "");
+   },
+
+   getString : function(value) {
+      return this.root.getCharPref(value);
+   },
+
+   setString : function(key, value) {
+      return this.root.setCharPref(key, value);
+   }
+};
 
 var simplenote = {
+
+  login: function() {
+    usr = simplenotePrefs.getString("username");
+    passwd = simplenotePrefs.getString("passwd");
+    reqBody = 'email='+usr+'&password='+passwd
+  
+    url = "https://simple-note.appspot.com/api/login"
+    encodedData = window.btoa(reqBody);
+  
+    let request = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]
+                .createInstance(Components.interfaces.nsIXMLHttpRequest);
+    request.onload = function(aEvent) {
+      authtok = aEvent.target.responseText;
+      simplenotePrefs.setString("authtok", authtok);
+    };
+    request.open("POST", url, true);
+    request.send(encodedData);
+    //setTimeout(function(){alert('authtok pref: ' + simplenotePrefs.getString("authtok"));}, 1000);
+  },
+
+  loadNotes: function() {
+    notesToLoad = 100; //TODO : Retrieve from preference
+
+    // https://simple-note.appspot.com/api2/index?length=[number of notes]&mark=[bookmark key]&since=[time value]&auth=[auth token]&email=[email]
+    url = "https://simple-note.appspot.com/api2/index?length="+notesToLoad;
+
+    let request = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]
+                .createInstance(Components.interfaces.nsIXMLHttpRequest);
+    request.onload = function(aEvent) {
+      responseText = aEvent.target.responseText;
+      alert('Response :' + responseText);
+    };
+    request.onerror = function(aEvent) {
+      errorText = aEvent.target.responseText;
+      alert('Error connecting to URL: '+ errorText);
+    };
+
+    request.open("GET", url, true);
+    request.send(null);
+  },
+
   newNote: function() {
-    alert("New Note");
+    document.getElementById("simplenote.mainwindow.notedata").value = "";
   },
 
   saveNote: function() {
-      alert("Save Note");
+    //Create JSON like {"content" : "New note!"}
+    userEnteredText = document.getElementById("simplenote.mainwindow.notedata").value
+
+    if (userEnteredText.length <=0) {
+      alert("Please enter data first");
+      return;
+    }
+
+    var noteBody = '{"content" : "' + userEnteredText +'" }';
+
+    url = "https://simple-note.appspot.com/api2/data?auth="+
+           simplenotePrefs.getString("authtok") + "&email="+
+           simplenotePrefs.getString("username");
+
+    let request = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]
+                .createInstance(Components.interfaces.nsIXMLHttpRequest);
+    request.onload = function(aEvent) {
+      newNoteKey = aEvent.target.responseText;
+      alert('New Note Key :' + newNoteKey);
+    };
+    request.onerror = function(aEvent) {
+      newNoteKey = aEvent.target.responseText;
+      alert('Error connecting to URL');
+    };
+    request.open("POST", url, true);
+    request.send(noteBody);
+    //setTimeout(function(){alert('Saved');}, 1000);
   },
 
   deleteNote: function() {
@@ -22,3 +105,9 @@ var simplenote = {
     }
   }
 };
+
+function onLoad(){
+  simplenotePrefs.init();
+  simplenote.login();
+  //simplenote.loadNotes();
+}
