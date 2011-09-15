1bb83b36577fcce8a5076f3d473f83a59a28a8fb
Mandar Vaze <mandarvaze@gmail.com>
2011-09-15 20:09:21 +0530

    Note list populated with first line of the note

--- chrome/content/simplenote.js
+++ chrome/content/simplenote.js
@@ -18,9 +18,46 @@ var simplenotePrefs = {
    }
 };
 
+var newnoteflag = false;
+var notes = {};
+var loggedIn = false;
+
+function logMsg(msg) {
+  dump(msg + '\n');
+}
+
+function fetchNote(Note) {
+  url = "https://simple-note.appspot.com/api2/data/"+Note.key
+  //logMsg('Loading :' + url + '\n');
+
+  let request = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]
+              .createInstance(Components.interfaces.nsIXMLHttpRequest);
+
+  request.onload = function(aEvent) {
+    noteListbox = document.getElementById("simplenote.mainwindow.notelist");
+    responseText = aEvent.target.responseText;
+    resp = JSON.parse(responseText);
+    notes[resp.key].content = resp.content;
+    header = notes[resp.key].content.split('\n')[0];
+    // CODESMELL: Running this multiple times is dangerous, will keep adding duplicate entries
+    noteListbox.appendItem(header, resp.key);
+  };
+
+  request.onerror = function(aEvent) {
+    errorText = aEvent.target.responseText;
+    alert('Error connecting to URL: '+ errorText);
+  };
+
+  request.open("GET", url, true);
+  request.send(null);
+}
+
 var simplenote = {
 
   login: function() {
+    // TODO : Check if we've already logged in
+    // No need to login every time
+
     usr = simplenotePrefs.getString("username");
     passwd = simplenotePrefs.getString("passwd");
     reqBody = 'email='+usr+'&password='+passwd
@@ -33,31 +70,47 @@ var simplenote = {
     request.onload = function(aEvent) {
       authtok = aEvent.target.responseText;
       simplenotePrefs.setString("authtok", authtok);
+      loggedIn = true;
+      logMsg('Login successful.')
     };
+    request.onerror = function(aEvent) {
+      errorText = aEvent.target.responseText;
+      alert('Unable to login: '+ errorText);
+    };
+
     request.open("POST", url, true);
     request.send(encodedData);
     //setTimeout(function(){alert('authtok pref: ' + simplenotePrefs.getString("authtok"));}, 1000);
   },
 
   loadNotes: function() {
+    // TODO : Check if we've already logged in
+    // if not, try logging in, else error
+
     notesToLoad = 100; //TODO : Retrieve from preference
 
     // https://simple-note.appspot.com/api2/index?length=[number of notes]&mark=[bookmark key]&since=[time value]&auth=[auth token]&email=[email]
     url = "https://simple-note.appspot.com/api2/index?length="+notesToLoad;
-    //alert('Loading notes from :' + url);
+    logMsg('Loading notes from :' + url);
 
     let request = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]
                 .createInstance(Components.interfaces.nsIXMLHttpRequest);
     request.onload = function(aEvent) {
       responseText = aEvent.target.responseText;
       resp = JSON.parse(responseText);
+      logMsg('Number of notes :' + resp.count)
       noteListbox = document.getElementById("simplenote.mainwindow.notelist");
       for (var i=0;i<resp.count;i++) {
-        if (resp.data[i].deleted == 0)
-          noteListbox.appendItem('val '+resp.data[i].key, resp.data[i].key);
+        if (resp.data[i].deleted == 0) {
+          key = resp.data[i].key;
+          note = resp.data[i];
+
+          notes[key] = note;
+          fetchNote(note);
+        }
       }
-      //alert('Note Count:' + resp.count);
     };
+
     request.onerror = function(aEvent) {
       errorText = aEvent.target.responseText;
       alert('Error connecting to URL: '+ errorText);
@@ -70,14 +123,18 @@ var simplenote = {
   newNote: function() {
     noteTextbox = document.getElementById("simplenote.mainwindow.notedata");
     noteTextbox.value = "";
+    newnoteflag = true;
   },
 
   saveNote: function() {
     noteTextbox = document.getElementById("simplenote.mainwindow.notedata");
-    userEnteredText = noteTextbox.value
+    userEnteredText = noteTextbox.value //BUG : Multiple lines not read
 
     if (userEnteredText.length <=0) {
       alert("Note is empty. Enter some text and then Save.");
+      return;
+    } else if (newnoteflag == false) {
+      alert("Updating existing notes not yet implemented.");
       return;
     }
 
@@ -89,6 +146,7 @@ var simplenote = {
 
     let request = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]
                 .createInstance(Components.interfaces.nsIXMLHttpRequest);
+
     request.onload = function(aEvent) {
       newNoteKey = aEvent.target.responseText;
       resp = JSON.parse(responseText);
@@ -98,8 +156,8 @@ var simplenote = {
         noteListbox.insertItemAt(0,'val '+resp.data[i].key, resp.data[i].key);
         noteListbox.selectedIndex = 0;
       }
-      //alert('New Note Key :' + newNoteKey);
     };
+
     request.onerror = function(aEvent) {
       newNoteKey = aEvent.target.responseText;
       alert('Error connecting to URL');
@@ -119,36 +177,18 @@ var simplenote = {
       alert("Good choice, Not deleting the note");
     }
   },
-  
-  fetchNote: function() {
-    noteListbox = document.getElementById("simplenote.mainwindow.notelist");
+
+  populateTextbox: function() {
     noteTextbox = document.getElementById("simplenote.mainwindow.notedata");
-    url = "https://simple-note.appspot.com/api2/data/"+this.value
-    // alert('Loading :' + url);
-
-    let request = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]
-                .createInstance(Components.interfaces.nsIXMLHttpRequest);
-
-    request.onload = function(aEvent) {
-      responseText = aEvent.target.responseText;
-      //alert(responseText);
-      resp = JSON.parse(responseText);
-      noteTextbox.value = resp.content;
-    };
-
-    request.onerror = function(aEvent) {
-      errorText = aEvent.target.responseText;
-      alert('Error connecting to URL: '+ errorText);
-    };
-
-    request.open("GET", url, true);
-    request.send(null);
+    content = notes[this.value].content;
+    if (content != undefined)
+      noteTextbox.value = content;
   }
 };
 
 function onLoad(){
   simplenotePrefs.init();
   simplenote.login();
-  document.getElementById("simplenote.mainwindow.notelist").onselect = simplenote.fetchNote
+  document.getElementById("simplenote.mainwindow.notelist").onselect = simplenote.populateTextbox
   simplenote.loadNotes();
 }
